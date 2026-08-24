469a5c7dec6935cadb9b7f7d493e9560e17fe3bf
sfox2006 <samfoxbartending@gmail.com>
2026-08-24 20:52:40 +0100

    Connect production Apps Script API

--- js/config.js
+++ js/config.js
@@ -1,6 +1,5 @@
 // Replace this value after deploying apps-script/Code.gs as a Google Apps Script web app.
 window.YEN_CONFIG = {
-  API_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxwURWEh_3ywtkh98ofAReV9ccZ4TyvJFnbqwaQfdeESc0qisrtlf_0HaFRP1ikFbhq/exec',
   REQUEST_TIMEOUT_MS: 20000
 };
-
